a75838118dfd9b6992acbf8f4d41a1d768e70179
128P <color22822@gmail.com>
2026-04-07 12:51:57 +0300

    дополнение main формы

--- WindowsFormsApp1/Forms/MainForm.Designer.cs
+++ WindowsFormsApp1/Forms/MainForm.Designer.cs
@@ -6,8 +6,15 @@
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.DataGridView dgvClients;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnAddClient;
+        private System.Windows.Forms.Button btnEditClient;
+        private System.Windows.Forms.Button btnDeleteClient;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Label lblStatus;
+
 
         /// <summary>
         /// Освободить все используемые ресурсы.
@@ -30,38 +37,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblWelcome = new System.Windows.Forms.Label();
+            this.dgvClients = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnAddClient = new System.Windows.Forms.Button();
+            this.btnEditClient = new System.Windows.Forms.Button();
+            this.btnDeleteClient = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).BeginInit();
             this.SuspendLayout();
-            // 
+
             // lblWelcome
-            // 
             this.lblWelcome.AutoSize = true;
-            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.lblWelcome.Location = new System.Drawing.Point(60, 78);
-            this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(158, 20);
-            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblWelcome.Location = new System.Drawing.Point(12, 9);
+            this.lblWelcome.Size = new System.Drawing.Size(150, 20);
             this.lblWelcome.Text = "Добро пожаловать!";
-            // 
+
+            // dgvClients
+            this.dgvClients.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvClients.Location = new System.Drawing.Point(12, 50);
+            this.dgvClients.Size = new System.Drawing.Size(860, 350);
+            this.dgvClients.ReadOnly = true;
+            this.dgvClients.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvClients.MultiSelect = false;
+
+            // btnRefresh
+            this.btnRefresh.Location = new System.Drawing.Point(12, 410);
+            this.btnRefresh.Size = new System.Drawing.Size(100, 35);
+            this.btnRefresh.Text = "Обновить";
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+
+            // btnAddClient
+            this.btnAddClient.Location = new System.Drawing.Point(120, 410);
+            this.btnAddClient.Size = new System.Drawing.Size(100, 35);
+            this.btnAddClient.Text = "Добавить";
+
+            // btnEditClient
+            this.btnEditClient.Location = new System.Drawing.Point(228, 410);
+            this.btnEditClient.Size = new System.Drawing.Size(100, 35);
+            this.btnEditClient.Text = "Изменить";
+
+            // btnDeleteClient
+            this.btnDeleteClient.Location = new System.Drawing.Point(336, 410);
+            this.btnDeleteClient.Size = new System.Drawing.Size(100, 35);
+            this.btnDeleteClient.Text = "Удалить";
+
             // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(178, 225);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(120, 40);
-            this.btnExit.TabIndex = 0;
+            this.btnExit.Location = new System.Drawing.Point(772, 410);
+            this.btnExit.Size = new System.Drawing.Size(100, 35);
             this.btnExit.Text = "Выход";
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
+
+            // lblStatus
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 455);
+            this.lblStatus.Text = "Готово";
+
             // MainForm
-            // 
-            this.ClientSize = new System.Drawing.Size(514, 361);
+            this.ClientSize = new System.Drawing.Size(884, 490);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDeleteClient);
+            this.Controls.Add(this.btnEditClient);
+            this.Controls.Add(this.btnAddClient);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dgvClients);
             this.Controls.Add(this.lblWelcome);
-            this.Name = "MainForm";
-            this.Text = "CRM Система";
+            this.Text = "CRM Система - Управление клиентами";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
 
         }
 
--- WindowsFormsApp1/Forms/MainForm.cs
+++ WindowsFormsApp1/Forms/MainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp1.Forms;
+using WindowsFormsApp1.Helpers;
 
 namespace WindowsFormsApp1
 {
@@ -16,16 +17,45 @@ namespace WindowsFormsApp1
         public MainForm()
         {
             InitializeComponent();
-            UpdateWelcomeMessage();
+            CheckAccessRights();
+            LoadClients();
+
         }
 
-        private void UpdateWelcomeMessage()
+        private void CheckAccessRights()
         {
-            lblWelcome.Text = $"Добро пожаловать, {LoginForm.CurrentUsername} (роль: {LoginForm.CurrentUserRole})";
+            if (LoginForm.CurrentUserRole == "Client")
+            {
+                btnAddClient.Enabled = false;
+                btnEditClient.Enabled = false;
+                btnDeleteClient.Enabled = false;
+            }
+        }
+
+        private void LoadClients()
+        {
+            try
+            {
+                string query = "SELECT ClientID, FullName, Phone, Email, Company FROM Clients";
+                DataTable dt = DatabaseHelper.ExecuteQuery(query);
+                dgvClients.DataSource = dt;
+
+                lblStatus.Text = $"Загружено клиентов: {dt.Rows.Count}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки клиентов: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadClients();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            DatabaseHelper.LogAction(LoginForm.CurrentUserID, "Logout", "Users", LoginForm.CurrentUserID);
             Application.Exit();
         }
     }
